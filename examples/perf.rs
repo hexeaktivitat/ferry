3f0a9cf481543c07496bf529b32243809d77d805
hexeaktivitat <hexeaktivitat@gmail.com>
2024-09-21 17:14:40 -0400

    load external files for perf.rs

--- examples/perf.rs
+++ examples/perf.rs
@@ -1,71 +1,37 @@
 // simple manual performance checks
 
-use std::time::Instant;
+use std::{fs, time::Instant};
 
 fn main() {
+    hello();
     euler_1();
     fib();
 }
 
-fn euler_1() {
-    let euler_1: String = "
-        def fn abs(a: Int) -> Int: (
-            if (a < 0) then: a * (0 - 1)
-            else: a
-        )
-
-        def fn modulo(a: Int, b: Int) -> Int: (
-            if (a - b) < b then: (
-                abs(a - b)
-            ) else: (
-                modulo(abs(a-b), b)
-            )
-        )
-
-        // modulo operator needed for this
-
-        let sum: Int = 0
-
-        for n: Int in 1..1000: 
-            if modulo(n, 3) == 0 then: 
-                sum = sum + n
-            else: (
-                if modulo(n, 5) == 0 then: 
-                    sum = sum + n
-            )
-
-        sum"
-    .into();
-
-    let mut program = ferry::Ferry::new(euler_1);
-
+fn hello() {
+    let source: String =
+        fs::read_to_string("examples/hello_ferry.feri").expect("file should exist");
+    let mut program = ferry::Ferry::new(source);
     let start_time = Instant::now();
-
     let output = program.run();
-
     let elapsed_time = start_time.elapsed().as_secs_f64();
+    println!("{} in: {elapsed_time}s", output.unwrap());
+}
 
+fn euler_1() {
+    let source: String = fs::read_to_string("examples/euler1.feri").expect("file should exist");
+    let mut program = ferry::Ferry::new(source);
+    let start_time = Instant::now();
+    let output = program.run();
+    let elapsed_time = start_time.elapsed().as_secs_f64();
     println!("{} in: {elapsed_time}s", output.unwrap());
 }
 
 fn fib() {
-    let source: String = "
-        def fn fib(n: Int) -> Int: (
-            if n <= 1 then:
-                n
-            else: (fib(n - 2) + fib(n - 1))
-        )
-
-        fib(30)"
-        .into();
-
+    let source: String = fs::read_to_string("examples/fib.feri").expect("file should exist");
     let mut program = ferry::Ferry::new(source);
-
     let start_time = Instant::now();
-
     let output = program.run();
-
     let elapsed_time = start_time.elapsed().as_secs_f64();
-
     println!("{} in: {elapsed_time}s", output.unwrap());
 }
