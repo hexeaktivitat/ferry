39c63cfb3855f0ee5d8d9f73f422f0bc53f4af0d
hexeaktivitat <hexeaktivitat@gmail.com>
2024-09-29 10:08:38 -0400

    addtl opcodes

--- src/ir.rs
+++ src/ir.rs
@@ -4,7 +4,8 @@ use miette::{Diagnostic, Result, SourceSpan};
 use thiserror::Error;
 
 use crate::{
-    state::{FerryState, FerryValue},
+    interpreter::FerryInterpreter,
+    state::{Convertable, FerryState, FerryValue},
     syntax::{
         walk_expr, Assign, Binary, Binding, Call, Expr, ExprVisitor, For, Function, Group, If, Lit,
         Loop, Unary, Variable,
@@ -20,6 +21,7 @@ pub enum FerryIrError {}
 
 // Type alias for addressing constants, etc.
 // Used for things that live outside the stack
+pub type FerryAddr = u8;
 
 #[derive(Debug, Clone)]
 pub enum FerryOpcode {
@@ -35,6 +37,7 @@ pub enum FerryOpcode {
     Sub,
     Mul,
     Div,
+    Alloc(FerryAddr, FerryValue),
 }
 
 // Into over From due to not being able to effeciently map u8 to fixed enum values
@@ -44,10 +47,11 @@ impl Into<u8> for FerryOpcode {
         match self {
             FerryOpcode::Nop => 0x00,
             FerryOpcode::Load(_) => 0x01,
-            FerryOpcode::Add => 0x02,
-            FerryOpcode::Sub => 0x03,
-            FerryOpcode::Mul => 0x04,
-            FerryOpcode::Div => 0x05,
+            FerryOpcode::Alloc(_, _) => 0x02,
+            FerryOpcode::Add => 0x10,
+            FerryOpcode::Sub => 0x11,
+            FerryOpcode::Mul => 0x12,
+            FerryOpcode::Div => 0x13,
             FerryOpcode::Return => 0xfe,
             FerryOpcode::Halt => 0xff,
         }
@@ -59,6 +63,7 @@ pub struct FerryIr {
     // AST to be lowered to this IR
     ast: Vec<Expr>,
     constants: Vec<FerryValue>,
+    heap_ptr: FerryAddr,
 }
 
 type FerryResult<T> = Result<T, FerryIrError>;
@@ -68,38 +73,39 @@ impl FerryIr {
         Self {
             ast,
             constants: vec![],
+            heap_ptr: 0x00,
         }
     }
 
-    pub fn lower(&mut self) -> FerryResult<Vec<FerryOpcode>> {
+    pub fn lower(&mut self, state: &mut FerryState) -> FerryResult<Vec<FerryOpcode>> {
         let mut program = vec![];
-        let mut state = vec![];
 
         for expr in self.ast.clone().iter_mut() {
-            match self.assemble_opcode(expr, &mut state) {
+            match self.assemble_opcode(expr, state) {
                 Ok(mut instructions) => program.append(&mut instructions),
                 Err(e) => println!("{e}"),
             }
         }
 
-        program.push(FerryOpcode::Halt);
+        program.push(FerryOpcode::Return);
+
         Ok(program)
     }
 
     fn assemble_opcode(
         &mut self,
         expr: &mut Expr,
-        state: &mut Vec<FerryValue>,
+        state: &mut FerryState,
     ) -> FerryResult<Vec<FerryOpcode>> {
         walk_expr(&mut *self, expr, state)
     }
 }
 
-impl ExprVisitor<FerryResult<Vec<FerryOpcode>>, &mut Vec<FerryValue>> for &mut FerryIr {
+impl ExprVisitor<FerryResult<Vec<FerryOpcode>>, &mut FerryState> for &mut FerryIr {
     fn visit_literal(
         &mut self,
         literal: &mut Lit,
-        state: &mut Vec<FerryValue>,
+        state: &mut FerryState,
     ) -> FerryResult<Vec<FerryOpcode>> {
         match literal {
             // treat undefined as a 0 for now
@@ -115,7 +121,14 @@ impl ExprVisitor<FerryResult<Vec<FerryOpcode>>, &mut Vec<FerryValue>> for &mut F
                 value,
                 expr_type,
                 span,
-            } => todo!(),
+            } => {
+                let ptr = self.heap_ptr;
+                self.heap_ptr += 1;
+                Ok(vec![
+                    FerryOpcode::Alloc(ptr, FerryValue::Str(value.clone())),
+                    FerryOpcode::Load(ptr as i64),
+                ])
+            }
             Lit::Bool {
                 token,
                 value,
@@ -127,14 +140,24 @@ impl ExprVisitor<FerryResult<Vec<FerryOpcode>>, &mut Vec<FerryValue>> for &mut F
                 contents,
                 expr_type,
                 span,
-            } => todo!(),
+            } => {
+                let ptr = self.heap_ptr;
+                self.heap_ptr += 1;
+                let mut list_interpreter =
+                    FerryInterpreter::new(vec![Expr::Literal(literal.clone())]);
+                let values = list_interpreter.interpret(state).unwrap().unwrap();
+                Ok(vec![
+                    FerryOpcode::Alloc(ptr, values),
+                    FerryOpcode::Load(ptr as i64),
+                ])
+            }
         }
     }
 
     fn visit_binary(
         &mut self,
         binary: &mut Binary,
-        state: &mut Vec<FerryValue>,
+        state: &mut FerryState,
     ) -> FerryResult<Vec<FerryOpcode>> {
         match binary.operator.get_token_type() {
             crate::token::TokenType::Operator(op) => match op {
@@ -202,7 +225,7 @@ impl ExprVisitor<FerryResult<Vec<FerryOpcode>>, &mut Vec<FerryValue>> for &mut F
     fn visit_unary(
         &mut self,
         unary: &mut Unary,
-        state: &mut Vec<FerryValue>,
+        state: &mut FerryState,
     ) -> FerryResult<Vec<FerryOpcode>> {
         todo!()
     }
@@ -210,7 +233,7 @@ impl ExprVisitor<FerryResult<Vec<FerryOpcode>>, &mut Vec<FerryValue>> for &mut F
     fn visit_variable(
         &mut self,
         variable: &mut Variable,
-        state: &mut Vec<FerryValue>,
+        state: &mut FerryState,
     ) -> FerryResult<Vec<FerryOpcode>> {
         todo!()
     }
@@ -218,7 +241,7 @@ impl ExprVisitor<FerryResult<Vec<FerryOpcode>>, &mut Vec<FerryValue>> for &mut F
     fn visit_assign(
         &mut self,
         assign: &mut Assign,
-        state: &mut Vec<FerryValue>,
+        state: &mut FerryState,
     ) -> FerryResult<Vec<FerryOpcode>> {
         todo!()
     }
@@ -226,7 +249,7 @@ impl ExprVisitor<FerryResult<Vec<FerryOpcode>>, &mut Vec<FerryValue>> for &mut F
     fn visit_if_expr(
         &mut self,
         if_expr: &mut If,
-        state: &mut Vec<FerryValue>,
+        state: &mut FerryState,
     ) -> FerryResult<Vec<FerryOpcode>> {
         todo!()
     }
@@ -234,7 +257,7 @@ impl ExprVisitor<FerryResult<Vec<FerryOpcode>>, &mut Vec<FerryValue>> for &mut F
     fn visit_group(
         &mut self,
         group: &mut Group,
-        state: &mut Vec<FerryValue>,
+        state: &mut FerryState,
     ) -> FerryResult<Vec<FerryOpcode>> {
         todo!()
     }
@@ -242,7 +265,7 @@ impl ExprVisitor<FerryResult<Vec<FerryOpcode>>, &mut Vec<FerryValue>> for &mut F
     fn visit_binding(
         &mut self,
         binding: &mut Binding,
-        state: &mut Vec<FerryValue>,
+        state: &mut FerryState,
     ) -> FerryResult<Vec<FerryOpcode>> {
         todo!()
     }
@@ -250,7 +273,7 @@ impl ExprVisitor<FerryResult<Vec<FerryOpcode>>, &mut Vec<FerryValue>> for &mut F
     fn visit_loop(
         &mut self,
         loop_expr: &mut Loop,
-        state: &mut Vec<FerryValue>,
+        state: &mut FerryState,
     ) -> FerryResult<Vec<FerryOpcode>> {
         todo!()
     }
@@ -258,7 +281,7 @@ impl ExprVisitor<FerryResult<Vec<FerryOpcode>>, &mut Vec<FerryValue>> for &mut F
     fn visit_for(
         &mut self,
         for_expr: &mut For,
-        state: &mut Vec<FerryValue>,
+        state: &mut FerryState,
     ) -> FerryResult<Vec<FerryOpcode>> {
         todo!()
     }
@@ -266,7 +289,7 @@ impl ExprVisitor<FerryResult<Vec<FerryOpcode>>, &mut Vec<FerryValue>> for &mut F
     fn visit_function(
         &mut self,
         function: &mut Function,
-        state: &mut Vec<FerryValue>,
+        state: &mut FerryState,
     ) -> FerryResult<Vec<FerryOpcode>> {
         todo!()
     }
@@ -274,7 +297,7 @@ impl ExprVisitor<FerryResult<Vec<FerryOpcode>>, &mut Vec<FerryValue>> for &mut F
     fn visit_call(
         &mut self,
         call: &mut Call,
-        state: &mut Vec<FerryValue>,
+        state: &mut FerryState,
     ) -> FerryResult<Vec<FerryOpcode>> {
         todo!()
     }
