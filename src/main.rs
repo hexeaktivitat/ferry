48f5cc727fa953022cd72a7d7e467df21566e424
hexeaktivitat <hexeaktivitat@gmail.com>
2024-10-20 20:44:39 -0400

    version up

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,7 @@ fn repl() -> Result<(), Error> {
     let mut input = String::new();
     let mut program = Ferry::new(input.clone());
 
-    println!("\nFERRY 0.2.3");
+    println!("\nFERRY 0.2.4");
     println!("===========");
     println!("Language Design Experiment");
     println!("Type !help for commands, or !quit to quit\n");
